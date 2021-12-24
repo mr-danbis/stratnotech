2436dbc2ace8aee9f8133a1211c64b8701b51f33
Daniel <dan.bisyukov@gmail.com>
2021-12-24 15:05:18 +0300

    created adaptive sidebar

--- app/js/main.js
+++ app/js/main.js
@@ -5,3 +5,19 @@ $(document).ready(function(){
     margin: 50,
   });
 });
+
+
+//Sidebar
+$('.header__sidebar-btn').on('click', function () {
+  if ($('.sidebar').hasClass('sidebar--hidden')) {
+    $('.sidebar').removeClass('sidebar--hidden');
+  }
+});
+
+$('.sidebar__close-btn').on('click', function () {
+  if ($('.sidebar').hasClass('sidebar--hidden')) {
+    $('.sidebar').removeClass('sidebar--hidden');
+  } else{
+    $('.sidebar').addClass('sidebar--hidden');
+  }
+});
