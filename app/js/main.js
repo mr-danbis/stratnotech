c1faca5555c2716cfcf3845ea3f25a554a832c5f
Daniel <dan.bisyukov@gmail.com>
2022-01-03 09:47:58 +0300

    added submenu to sidebar

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,7 @@
 $.noConflict();
-$(document).ready(function(){
+$(document).ready(function () {
   $(".slider").owlCarousel({
-    items:1,
+    items: 1,
     margin: 50,
   });
 });
@@ -17,7 +17,18 @@ $('.header__sidebar-btn').on('click', function () {
 $('.sidebar__close-btn').on('click', function () {
   if ($('.sidebar').hasClass('sidebar--hidden')) {
     $('.sidebar').removeClass('sidebar--hidden');
-  } else{
+  } else {
     $('.sidebar').addClass('sidebar--hidden');
   }
+});
+
+
+$('.sidebar__submenu').hide();
+
+$(".header__menu-link--notactive").click(function (event) {
+  event.preventDefault();
+});
+
+$('.sidebar__menu-link').click(function (event) {
+  $(this).next().slideToggle(300);
 });
